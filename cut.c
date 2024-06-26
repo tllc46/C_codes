3dcf88098c8e945f224a06011fc512065d53c6d9
tllc46 <155712238+tllc46@users.noreply.github.com>
2024-06-26 09:39:07 +0900

    Update cut.c

--- cut.c
+++ cut.c
@@ -10,7 +10,7 @@ gcc -o program cut.c $(bash sac-config -c -l sac sacio) -lm
 #include <sacio.h> //필수
 #include <sac.h> //필수
 
-extern sac *current; //get, set 함수를 쓰지 않고 바로 sac 구조체 활용 가능
+extern sac *current; //get, set 함수를 쓰지 않고 바로 SAC 구조체 활용 가능
 int main(int argc,char **argv)
 {
 	int max=400000; //npts보다 충분히 큰 수
@@ -20,7 +20,7 @@ int main(int argc,char **argv)
 	char filer[40]="HL.H35..HHZ.D.2021.276.000000.SAC",filew[10]="foo.sac";
 	rsac1(filer,y,&npts,&b,&delta,&max,&nerr,strlen(filer)); //sac IO 함수
 
-	cut(y,npts,b,delta,21500,21600,1,out,&nout); //sac library 함수
+	cut(y,npts,b,delta,21500,21600,1,out,&nout); //SAC library 함수
 
 	printf("npts=%d\n",current->h->npts);
 	printf("b=%f\n",current->h->_b);
@@ -30,7 +30,7 @@ int main(int argc,char **argv)
 
 	current->h->npts=nout; //sac 구조체 활용
 	current->z->_b=21500.0; //sac 구조체 활용
-	wsac0(filew,x,out,&nerr,strlen(filew)); //sac IO 함수
+	wsac0(filew,x,out,&nerr,strlen(filew)); //SAC IO 함수
 
 	return 0;
 }
